94229dba51e8918d117e144e4ba7956f8715f2da
Palm <palmpasuthorn@gmail.com>
2022-08-10 22:24:39 +0700

    style: Change Navbar icons

--- src/pages/with/WithBottomNavbar.tsx
+++ src/pages/with/WithBottomNavbar.tsx
@@ -1,5 +1,5 @@
-import { FaBackward, FaGithub } from 'react-icons/fa'
-import { BsPeopleFill } from 'react-icons/bs'
+import { BiArrowBack } from 'react-icons/bi'
+import { BsPeopleFill, BsGithub } from 'react-icons/bs'
 import { Outlet } from 'react-router-dom'
 import { NavContainer, NavIconButton } from '../../components/ui/Navigation'
 
@@ -10,10 +10,10 @@ export default function WithBottomNavbar({ showBackButton = false }: { showBackB
             <Outlet />
             {!showBackButton || <NavContainer location='left'>
                 { /* eslint-disable-next-line no-restricted-globals */ }
-                <NavIconButton action={() => history.back()} icon={FaBackward} />
+                <NavIconButton action={() => history.back()} icon={BiArrowBack} />
             </NavContainer>}
             <NavContainer location='right'>
-                <NavIconButton icon={FaGithub} to='https://github.com/PalmDevs/nrvm' external />
+                <NavIconButton icon={BsGithub} to='https://github.com/PalmDevs/nrvm' external />
                 <NavIconButton icon={BsPeopleFill} to='/about' />
             </NavContainer>
         </>
