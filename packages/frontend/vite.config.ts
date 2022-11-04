ef395141a8e3eadb9309239f9c71025420ba4034
Palm <palmpasuthorn@gmail.com>
2022-11-04 13:33:47 +0700

    chore(frontend): update ports when starting server

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3030
+  },
+  preview: {
+    port: 8080,
+    strictPort: true
+  },
   define: {
     'import.meta.env.APP_VERSION': `"${process.env.npm_package_version}"`
   }
